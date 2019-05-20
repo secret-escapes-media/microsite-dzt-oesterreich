cddfa1b767940e66c303e468ea0dcf688508b335
Birgit <birgit.voslamber@secretescapes.com>
2019-05-20 17:13:45 +0200

    new structure

--- gulpfile.js
+++ gulpfile.js
@@ -39,7 +39,7 @@ gulp.task('serve', ['build-jekyll'], function() {
     server: {
       baseDir: '_site/',
       routes: {
-        '/de/australien-2019': '_site/'
+        '/at/deutschland-2019': '_site/'
       }
     }
   });
